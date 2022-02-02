80d03627ec0b4fa65cc74afa483e23332f5aa6f1
AliwerDev <alwerdev@gmail.com>
2022-02-02 18:38:43 +0500

    rout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Home from './pages/home';
+import {Route, Routes} from "react-router-dom";
+import CodePage from "./pages/CodePage";
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <Routes>
+        <Route path={"/"} element={<Home/>}/>
+        <Route path={"/auth"} element={<p>Auth</p>}/>
+        <Route path={"/codePage"} element={<CodePage/>}/>
+    </Routes>
   );
 }
 
