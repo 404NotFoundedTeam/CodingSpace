72a02c5238e46d1e4586ae6e39000af3987e3cea
AliwerDev <alwerdev@gmail.com>
2022-02-03 13:50:37 +0500

    code editorga ishlov berildi

--- src/components/Editor/EditorWrapper.js
+++ src/components/Editor/EditorWrapper.js
@@ -4,9 +4,36 @@ import styled from 'styled-components'
 const EditorWrapper = styled.div`
   width: 100%;
   background: red;
+  
+  .copy-btn{
+    position:relative;
+    &:hover{
+      .copy-modal{
+        opacity: 1;
+        visibility: visible;
+      }
+    }
+    .copy-modal{
+      transition: all .3s;
+      opacity: 0;
+      visibility: collapse;
+      position:absolute;
+		top: 10px;
+      right: 60px;
+      padding: 3px 0;
+      width: 100px;
+      font-size: 12px;
+      color: #fff;
+      background: rgba(21, 50, 52, 0.4);
+      backdrop-filter: blur(2.8px);
+      -webkit-backdrop-filter: blur(2.8px);
+      z-index: 99999999;
+    }
+  }
 
   .code-mirror-wrapper {
-    font-size: 18px !important;
+    font-size: 12px !important;
+    letter-spacing: 1px;
   }
 `
 
